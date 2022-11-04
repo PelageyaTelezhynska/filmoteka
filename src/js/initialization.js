5e3ab745a33c5c80a92f3696f2fabe94d534c566
Sergey-Ryabko-84 <amigoswow@ukr.net>
2022-11-04 18:08:06 +0200

    fix fetch-trending

--- src/js/initialization.js
+++ src/js/initialization.js
@@ -2,8 +2,11 @@ import { markupTrending } from './render-trending'
 import { fetchTrending } from './fetch-trending'
 
 export function init() {
-    fetchTrending().then(data => {        
-        // console.log(data.results);
+    fetchTrending().then(data => {
+        data.results.map(elem => {
+            if (elem.release_date) elem.release_date = elem.release_date.slice(0, 4);
+            if (elem.first_air_date) elem.first_air_date = elem.first_air_date.slice(0, 4);
+        })
         markupTrending(data.results);
     })    
 }
