4d84038754b3e2299c57c60175063113e3efb53a
KristinaBatyo <105168024+KristinaBatyo@users.noreply.github.com>
2022-11-06 21:37:34 +0200

    correct

--- src/js/modal-film.js
+++ src/js/modal-film.js
@@ -1,9 +1,7 @@
 import { renderMarkupModal } from './render-modal';
 import { fetchModal } from './fetch-modal';
 import { spinnerOn, spinnerOff } from './spinner';
-const BASE_URL = 'https://api.themoviedb.org/3';
-const API_KEY = 'fa9433e46ed4abfaeb75bcf31f473feb';
-
+import YouTube from 'react-youtube';
 const refs = {
   openModalCard: document.querySelector('[data-modal-open]'),
   closeModalBtn: document.querySelector('[data-modal-close]'),
