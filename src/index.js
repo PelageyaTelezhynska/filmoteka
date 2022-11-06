c15ac5e19764c8459402c1d8c8eb48a25699ba3b
Tonya-Shyva <toshash69@gmail.com>
2022-11-06 19:23:32 +0200

    feature/day-night-theme

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './js/pagination';
 import './js/fetch-modal';
 import './js/modal-film';
 // import './js/render-storage';
+import './js/day-night-theme';
 
 savingGenresArr();
 pageRender(1);
--- src/js/page-render.js
+++ src/js/page-render.js
@@ -1,29 +1,33 @@
 import { renderMarkupTrending } from './render-trending';
 import { fetchTrending } from './fetch-trending';
 import { spinnerOn, spinnerOff } from './spinner';
+import { toggleLightTheme } from './day-night-theme';
+
 export function pageRender(pageNum) {
-
-    fetchTrending(pageNum).then(data => {
-        data.results.map(elem => {
-            if (elem.release_date) elem.release_date = elem.release_date.slice(0, 4);
-            if (elem.first_air_date) elem.first_air_date = elem.first_air_date.slice(0, 4);
-        })
-        const genresArr = JSON.parse(localStorage.getItem('genres'));
-        data.results.map((elem, idx) => {
-            const genresName = [];
-            elem.genre_ids.map((genreId) => {
-                const genreEl = genresArr.find(genre => genre.id === genreId);                
-                genresName.push(genreEl?.name);
-            });
-            const arrLength = genresName.length;
-            if (arrLength > 3){
-                genresName.splice(2, arrLength - 2, "Other")
-            }
-            data.results[idx].genres_name_str = genresName.join(', ');
-        })
-        console.log(data.results);
-        renderMarkupTrending(data.results);
-
+  fetchTrending(pageNum)
+    .then(data => {
+      data.results.map(elem => {
+        if (elem.release_date)
+          elem.release_date = elem.release_date.slice(0, 4);
+        if (elem.first_air_date)
+          elem.first_air_date = elem.first_air_date.slice(0, 4);
+      });
+      const genresArr = JSON.parse(localStorage.getItem('genres'));
+      data.results.map((elem, idx) => {
+        const genresName = [];
+        elem.genre_ids.map(genreId => {
+          const genreEl = genresArr.find(genre => genre.id === genreId);
+          genresName.push(genreEl?.name);
+        });
+        const arrLength = genresName.length;
+        if (arrLength > 3) {
+          genresName.splice(2, arrLength - 2, 'Other');
+        }
+        data.results[idx].genres_name_str = genresName.join(', ');
+      });
+      console.log(data.results);
+      renderMarkupTrending(data.results);
+      toggleLightTheme();
     })
     .finally(() => spinnerOff());
   spinnerOn();
