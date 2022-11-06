7d050eb7b32ecc21a0b64ab329c0a32561da6e5c
PelageyaTelezhynska <105298126+PelageyaTelezhynska@users.noreply.github.com>
2022-11-06 17:48:37 +0300

    Revert "Made a keyword query"

--- src/js/refs.js
+++
@@ -1,3 +0,0 @@
-export const refs = {
-  inputRef: document.querySelector('.search__input'),
-};
--- src/js/render-colection.js
+++
@@ -1,5 +0,0 @@
-import markup from './templates/film-card.hbs';
-
-export default function markupColection(data) {
-  document.querySelector('.movies__list').innerHTML = markup(data);
-}
