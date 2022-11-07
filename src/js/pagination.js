0228dbb0f9ef4b97ecdba991ac6ef1b030bb718a
Sergey-Ryabko-84 <amigoswow@ukr.net>
2022-11-07 11:45:53 +0200

    added save current page

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -2,36 +2,43 @@ import Pagination from 'tui-pagination';
 import { pageRender } from './page-render';
 import { fetchTrending } from './fetch-trending';
 
+let trendingTotalResults = 20000;
+
 fetchTrending(1).then(data => {
-  // Adds to localStorage the value of total_results
-  localStorage.setItem('trending_total_results', data.total_results);
-  setTotalItems();
+    trendingTotalResults = data.total_results;
+    setTotalItems();
 
-  function setTotalItems() {
-    const totalResults = localStorage.getItem('trending_total_results');
+    function setTotalItems () {
+        const totalResults = trendingTotalResults;
+    
+        if (totalResults) {
+            const pagination = new Pagination(document.getElementById('pagination'), {
+                totalItems: totalResults,
+                itemsPerPage: 20,
+                visiblePages: 5,
+                centerAlign: true,
+            });
+            
+            pagination.on('afterMove', function(eventData) {
+                pageRender(eventData.page);
+                localStorage.setItem("current_page", pagination.getCurrentPage());
+            });
 
-    if (totalResults) {
-      const pagination = new Pagination(document.getElementById('pagination'), {
-        totalItems: totalResults,
-        itemsPerPage: 20,
-        visiblePages: 5,
-        centerAlign: true,
-      });
+            pagination.movePageTo(localStorage.getItem('current_page'));
+        } else {
+            const pagination = new Pagination(document.getElementById('pagination'), {
+                totalItems: 20000,
+                itemsPerPage: 20,
+                visiblePages: 5,
+                centerAlign: true,
+            });
+            
+            pagination.on('afterMove', function(eventData) {
+                pageRender(eventData.page);
+                localStorage.setItem("current_page", pagination.getCurrentPage());
+            });
 
-      pagination.on('afterMove', function (eventData) {
-        pageRender(eventData.page);
-      });
-    } else {
-      const pagination = new Pagination(document.getElementById('pagination'), {
-        totalItems: 20000,
-        itemsPerPage: 20,
-        visiblePages: 5,
-        centerAlign: true,
-      });
-
-      pagination.on('afterMove', function (eventData) {
-        pageRender(eventData.page);
-      });
+            pagination.movePageTo(localStorage.getItem('current_page'));
+        }
     }
-  }
-});
+})
