3b6b6a414ace523a237e8673375115e3e086aa91
Tonya-Shyva <toshash69@gmail.com>
2022-11-07 18:11:52 +0200

    bugfixed day-night-theme
    and added paddind-left for .info when movie-card:hover

--- src/js/day-night-theme.js
+++ src/js/day-night-theme.js
@@ -7,16 +7,20 @@ export function toggleLightTheme() {
 
   function toggleThemeClick(e) {
     e.preventDefault();
-    if (e.target.nodeName === 'svg' || e.target.nodeName === 'use') {
+    if (
+      e.target.nodeName === 'svg' ||
+      e.target.nodeName === 'use' ||
+      e.target.nodeName === 'button'
+    ) {
       darkIcon.classList.toggle('is-shown');
 
       //   console.log(moviesTitleArr);
       if (darkIcon.classList.contains('is-shown')) {
-        console.log('dark true');
+        // console.log('dark true');
         document.body.classList.add('js-dark-theme');
         footerRef.classList.add('js-dark-theme');
       } else {
-        console.log('dark false');
+        // console.log('dark false');
         document.body.classList.remove('js-dark-theme');
         footerRef.classList.remove('js-dark-theme');
       }
