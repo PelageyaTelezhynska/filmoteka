d563606e8ddc6e146a97be4eda76845b7ee60f4e
Tonya-Shyva <toshash69@gmail.com>
2022-11-08 12:07:06 +0200

    Update pagination.js
    import togglrLightTheme and her challenge

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,45 +1,54 @@
 import Pagination from 'tui-pagination';
 import { pageRender } from './page-render';
+import { toggleLightTheme } from './day-night-theme';
 
 let trendingTotalResults = 20000;
 
-export function addPagination (fetchfoo) {
-    fetchfoo.then(data => {
-        trendingTotalResults = data.total_results;
-        setTotalItems();
+export function addPagination(fetchfoo) {
+  fetchfoo.then(data => {
+    trendingTotalResults = data.total_results;
+    setTotalItems();
 
-        function setTotalItems () {
-            const totalResults = trendingTotalResults;
-        
-            if (totalResults) {
-                const pagination = new Pagination(document.getElementById('pagination'), {
-                    totalItems: totalResults,
-                    itemsPerPage: 20,
-                    visiblePages: 5,
-                    centerAlign: true,
-                });
-                
-                pagination.on('afterMove', function(eventData) {
-                    pageRender(eventData.page);
-                    localStorage.setItem("current_page", pagination.getCurrentPage());
-                });
+    function setTotalItems() {
+      const totalResults = trendingTotalResults;
 
-                pagination.movePageTo(localStorage.getItem('current_page'));
-            } else {
-                const pagination = new Pagination(document.getElementById('pagination'), {
-                    totalItems: 20000,
-                    itemsPerPage: 20,
-                    visiblePages: 5,
-                    centerAlign: true,
-                });
-                
-                pagination.on('afterMove', function(eventData) {
-                    pageRender(eventData.page);
-                    localStorage.setItem("current_page", pagination.getCurrentPage());
-                });
+      if (totalResults) {
+        const pagination = new Pagination(
+          document.getElementById('pagination'),
+          {
+            totalItems: totalResults,
+            itemsPerPage: 20,
+            visiblePages: 5,
+            centerAlign: true,
+          }
+        );
 
-                pagination.movePageTo(localStorage.getItem('current_page'));
-            }
-        }
-    })
+        pagination.on('afterMove', function (eventData) {
+          pageRender(eventData.page);
+          toggleLightTheme();
+          localStorage.setItem('current_page', pagination.getCurrentPage());
+        });
+
+        pagination.movePageTo(localStorage.getItem('current_page'));
+      } else {
+        const pagination = new Pagination(
+          document.getElementById('pagination'),
+          {
+            totalItems: 20000,
+            itemsPerPage: 20,
+            visiblePages: 5,
+            centerAlign: true,
+          }
+        );
+
+        pagination.on('afterMove', function (eventData) {
+          pageRender(eventData.page);
+          toggleLightTheme();
+          localStorage.setItem('current_page', pagination.getCurrentPage());
+        });
+
+        pagination.movePageTo(localStorage.getItem('current_page'));
+      }
+    }
+  });
 }
