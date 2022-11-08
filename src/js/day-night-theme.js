7750b8f7e55b70654cfa318f510520d0dc32ef9b
Tonya-Shyva <toshash69@gmail.com>
2022-11-08 11:14:29 +0200

    finally code
    назви фільмів також змінюють колір

--- src/js/day-night-theme.js
+++ src/js/day-night-theme.js
@@ -1,7 +1,6 @@
 export function toggleLightTheme() {
   const themeContainer = document.querySelector('.themetoggle__container');
   const lightIcon = document.querySelector('.themetoggle__icon-light');
-  const footerRef = document.querySelector('.footer');
 
   themeContainer.addEventListener('click', toggleThemeClick);
 
@@ -13,16 +12,7 @@ export function toggleLightTheme() {
       e.target.nodeName === 'BUTTON'
     ) {
       lightIcon.classList.toggle('is-shown');
-      //   console.log(moviesTitleArr);
-      if (lightIcon.classList.contains('is-shown')) {
-        // console.log('dark true');
-        document.body.classList.add('js-dark-theme');
-        footerRef.classList.add('js-dark-theme');
-      } else {
-        // console.log('dark false');
-        document.body.classList.remove('js-dark-theme');
-        footerRef.classList.remove('js-dark-theme');
-      }
+      document.body.classList.toggle('js-dark-theme');
     }
   }
 }
