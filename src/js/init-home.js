e60c72deadbcd93e1cfc10ab95b31b019b1b955b
Sergey-Ryabko-84 <amigoswow@ukr.net>
2022-11-09 22:06:32 +0200

    adds murkup genre select

--- src/js/init-home.js
+++ src/js/init-home.js
@@ -1,7 +1,9 @@
 import { pageRender } from './page-render';
 import { savingGenresArr } from './saving-genres-arr';
 import {addPagination} from './pagination'
+import {renderGenreSelect} from './genre-select-render'
 
 savingGenresArr();
+renderGenreSelect();
 pageRender(1);
 addPagination();
