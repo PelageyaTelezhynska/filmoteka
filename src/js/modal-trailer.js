4a8c64459e2eccf5fb151b84480d2789a0405a75
KristinaBatyo <105168024+KristinaBatyo@users.noreply.github.com>
2022-11-13 02:00:57 +0200

    відтворення відео

--- src/js/modal-trailer.js
+++ src/js/modal-trailer.js
@@ -3,6 +3,7 @@ import { renderMarkupModal } from "./modal-film";
 import { spinnerOn, spinnerOff } from "./spinner";
 import * as basicLightbox from "basiclightbox";
 import axios from "axios";
+import Notiflix from "notiflix";
 import "basiclightbox/dist/basicLightbox.min.css";
 const BASE_URL = "https://api.themoviedb.org/3/movie/";
 const API_KEY = "fa9433e46ed4abfaeb75bcf31f473feb";
@@ -20,20 +21,28 @@ async function onTrailerClick() {
 
 
   async function fetchTrailer(currentId) {
+        try {
         const response = await axios.get(
             `${BASE_URL}${currentId}/videos?api_key=${API_KEY}&language=en-US`,
         );
         const key = response.data.results[0].key;
         renderTrailer(key);
+    } catch (error) {
+        Notiflix.Notify.failure("Sorry, there is no trailer for this movie");
     }
+}
 
-   async function renderTrailer(key) {
+    function renderTrailer(key) {
         const instance = basicLightbox.create(`
+        <div class="modal-trailer">
         <iframe width="560" height="315" src="https://www.youtube.com/embed/${key}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        `, {sameSite: 'none', secure: true});
+        </div>`);
         instance.show();
         
     }
+
+
+    
 
     
 
